f5cd2548fbb2234d861f6b96789e26a53bcf4de5
v-Alqorith <vahapcandongel@hotmail.com>
2023-01-30 21:57:55 +0000

    categories are now fetched within order

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,24 +1,13 @@
-export default function Categories() {
+export default function Categories({ data }) {
   return (
     <div className="w-full h-screen flex justify-center items-center gap-4">
-          <div className="bg-white w-[250px] h-[100px] rounded-md shadow-lg flex justify-center items-center hover:cursor-pointer">
-              <div className="text-black flex justify-center items-center text-[24px]">
-                  Kebab
-              </div>
+      {data.map((item) => (
+        <div className="bg-white w-[250px] h-[100px] rounded-md shadow-lg flex justify-center items-center hover:cursor-pointer">
+          <div className="text-black flex justify-center items-center text-[24px]">
+            {item.name}
           </div>
-
-          <div className="bg-white w-[250px] h-[100px] rounded-md shadow-lg flex justify-center items-center hover:cursor-pointer">
-              <div className="text-black flex justify-center items-center text-[24px]">
-                  Burger
-              </div>
-          </div>
-
-          <div className="bg-white w-[250px] h-[100px] rounded-md shadow-lg flex justify-center items-center hover:cursor-pointer">
-              <div className="text-black flex justify-center items-center text-[24px]">
-                  Chicken
-              </div>
-          </div>
-
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/order.js
+++ src/pages/order.js
@@ -1,11 +1,24 @@
 import Basket from "../components/Basket";
 import Categories from "../components/Categories";
+import { PrismaClient } from "@prisma/client";
 
-export default function Order() {
+const prisma = new PrismaClient();
+
+export default function Order({ data }) {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <Categories />
+      <Categories data={data} />
       <Basket />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const categories = await prisma.category.findMany();
+
+  return {
+    props: {
+      data: categories,
+    },
+  };
 }
